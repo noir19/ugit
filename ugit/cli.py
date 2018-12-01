65e47ccfaafe9f6eb998aee559ab93a43fcf8465
Nikita Leshenko <nikita@leshenko.net>
2018-12-01 22:06:01 +0200

    cat-file: Print hashed objects
    This command is the "opposite" of `hash-object`: it can print an object by its
    OID. Its implementation just reads the file at *.ugit/objects/{OID}*.
    
    The names `hash-object` and `cat-file` aren't the clearest of names, but they
    are the names that Git uses so we'll stick to them for consistency.
    
    We can now try the full cycle:
    
        $ cd /tmp/new
        $ ugit init
        Initialized empty ugit repository in /tmp/new/.ugit
        $ echo some file > bla
        $ ugit hash-object bla
        0e08b5e8c10abc3e455b75286ba4a1fbd56e18a5
        $ ugit cat-file 0e08b5e8c10abc3e455b75286ba4a1fbd56e18a5
        some file
    
    Note that the name of the file (*bla*) wasn't preserved as part of this process,
    because, again, the object database is just about storing bytes for later
    retrieval and it doesn't care which filename the bytes came from.
    
    %% METADATA
    Id: cat-file

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 from . import data
 
@@ -21,6 +22,10 @@ def parse_args ():
     hash_object_parser.set_defaults (func=hash_object)
     hash_object_parser.add_argument ('file')
 
+    cat_file_parser = commands.add_parser ('cat-file')
+    cat_file_parser.set_defaults (func=cat_file)
+    cat_file_parser.add_argument ('object')
+
     return parser.parse_args ()
 
 
@@ -31,3 +36,8 @@ def init (args):
 def hash_object (args):
     with open (args.file, 'rb') as f:
         print (data.hash_object (f.read ()))
+
+
+def cat_file (args):
+    sys.stdout.flush ()
+    sys.stdout.buffer.write (data.get_object (args.object))
